46b0125d6be7451d4a2168d332441f05ac5e6e14
ken1lnw <moowinza@gmail.com>
2023-07-04 15:07:07 +0700

    api user change

---
+++ src/pages/api/user/index.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { method } = req;
+
+    switch (method) {
+        case 'GET':
+            try {
+                const page: number = Number(req.query.page) || 1;
+                const pageSize: number = Number(req.query.pageSize) || 10;
+
+                const users = await prisma.user.findMany({
+                    skip: (page - 1) * pageSize,
+                    take: pageSize,
+                });
+
+                const totalusers = await prisma.user.count();
+                const totalPage: number = Math.ceil(totalusers / pageSize);
+                res.status(200).json({ users, page, pageSize, totalPage });
+            } catch (error) {
+                res.status(500).json({ error: "An error occurred while fetching the users" });
+            }
+            break;
+
+        case 'POST':
+            try {
+                const newuser = await prisma.user.create({
+                    data: req.body,
+                });
+
+                res.status(201).json(newuser);
+            } catch (error) {
+                res.status(500).json({ error: "An error occurred while creating the user" });
+            }
+            break;
+
+        default:
+            res.setHeader('Allow', ['GET', 'POST']);
+            res.status(405).end(`Method ${method} Not Allowed`);
+    }
+}
